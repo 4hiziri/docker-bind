a0d50abe709abc02eeab5583b60a33fefb9c90b8
4hiziri <meirvg@gmail.com>
2017-11-23 20:11:02 +0900

    Ommit webmin

--- update_pass.sh
+++ update_pass.sh
@@ -12,21 +12,3 @@ chown bind: /etc/bind/*
 chown bind: /etc/bind/
 chown bind: /var/lib/bind/*
 chown bind: /var/lib/bind/
-
-if [ ! -e /etc/webmin/miniserv.conf ]; then
-  cp -R /root/webmin/* /etc/webmin/ 
-fi
-
-# Honor the request to change the password
-
-if [ ! -z $PASS ]; then
-  echo root:$PASS | /usr/sbin/chpasswd
-fi
-
-# Add any networks to the allowed option
-
-if [ ! -z $NET ]; then
-  MYNET=${NET//;/\ }
-  cat /root/webmin/miniserv.conf > /etc/webmin/miniserv.conf
-  echo 'allow='$MYNET >> /etc/webmin/miniserv.conf
-fi
